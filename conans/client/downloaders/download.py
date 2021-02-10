a634d833ea40847639c024ac50ed6c730bfecbc1
Gonzalo Ruiz <gonso.ruiz@gmail.com>
2021-02-10 14:51:16 +0100

    End of session

--- conans/client/downloaders/download.py
+++ conans/client/downloaders/download.py
@@ -2,10 +2,10 @@ from conans.client.downloaders.cached_file_downloader import CachedFileDownloade
 from conans.client.downloaders.file_downloader import FileDownloader
 
 
-def run_downloader(requester, output, verify, config, user_download=False, use_cache=True, **kwargs):
+def run_downloader(requester, output, verify, config, user_download=False, use_cache=True, use_remote_cache=False, **kwargs):
     downloader = FileDownloader(requester=requester, output=output, verify=verify, config=config)
     if use_cache and config.download_cache:
-        if config.remote_cache:
+        if use_remote_cache and config.remote_cache:
             downloader = CachedFileDownloader(remote_cache=config.remote_cache, file_downloader=downloader,
                                               user_download=user_download)
         else:
